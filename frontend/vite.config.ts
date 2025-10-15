f6fc31609c632e4a0ff8e5fd645fd7afce9439a1
Yulia Ioffe <yumioffe@gmail.com>
2025-10-15 14:33:49 +0200

    fix frontend error by disabling auto browser load

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite';
+import fs from 'fs';
+
+// сhecking if it's docker run
+const isDocker = fs.existsSync('/.dockerenv');
 
 export default defineConfig({
-	root: '.',
-	publicDir: 'public',
-	server:
-	{
-		port: 3000,
-		open: true
-	}
+  root: '.',
+  publicDir: 'public',
+  server: {
+    port: 3000,
+    open: !isDocker // only locally open browser
+  },
+  preview: {
+    port: 3000,
+    host: true,
+    open: !isDocker // same behaviour for vite preview
+  }
 });
