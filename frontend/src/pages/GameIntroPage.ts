5e0e9ac32eb4940d5f10866e0cab3000acc2d6cc
Juan Aldana <martinzx13@github.com>
2025-10-18 23:20:54 +0100

    (add) config to tailwind file to try to use the variables

--- frontend/src/pages/GameIntroPage.ts
+++ frontend/src/pages/GameIntroPage.ts
@@ -2,9 +2,13 @@ import { sidebarDisplay } from "../components/SideBar"
 import { profileDivDisplay } from "../components/ProfileDiv"
 import { LogOutBtnDisplay } from "../components/LogOutBtn"
 import { addTheme } from "../components/Theme"
+import { QuickGameCard } from '../components/cards/QuickGameCard'
+import { ModeCards } from '../components/cards/ModeCards'
+import { autoConnect, sendWSMessage } from "../services/ws";
 
 // Adapted function now that data extraction has been centralized
 export function GameIntroPage() {
+    autoConnect(handleWSMessage);
     return `
 	<!-- Theme -->
 		${ addTheme() }
@@ -26,25 +30,21 @@ export function GameIntroPage() {
         <p class="text-lg text-gray-600 max-w-xl text-center mb-10">
 			Challenge a friend and prove your skills.
         </p>
-
-        <!-- Feature cards -->
-        <div class="flex gap-6 flex-wrap justify-center">
-          <div class="rounded-xl shadow-[0_0_30px_10px_#7037d3] p-6 w-64 text-center cursor-pointer hover:bg-purple-700"
-     			onclick="window.location.hash='lobby'">
- 			<h2 class="font-bold text-cyan-400 mb-2">👾 Player vs Player</h2>
-  			<p class="text-white text-sm">Classic 1v1 arcade action. Challenge a friend and see who's the best.</p>
-		</div>
-          <div class="rounded-xl shadow-[0_0_30px_10px_#7037d3] p-6 w-64 text-center cursor-pointer hover:bg-purple-700"
-		       	onclick="window.location.hash='lobbytournament'">
-            <h2 class="font-bold text-cyan-400 mb-2">🏆 Tournament Mode</h2>
-            <p class="text-white text-sm">A series of matches leading to a champion. Add your friends or guests to compete in an arcade battle.</p>
-          </div>
-          <div class="rounded-xl shadow-[0_0_30px_10px_#7037d3] p-6 w-64 text-center cursor-pointer hover:bg-purple-700"
-		  		 onclick="window.location.hash='AIopponent'">
-            <h2 class="font-bold text-cyan-400 mb-2">🤖 AI Opponent Mode</h2>
-            <p class="text-white text-sm">This mode is for practicing or battling solo against the computer.</p>
-          </div>
-        </div>
-      </div>
+        ${ QuickGameCard() }
+        <!-- Mode cards -->
+        ${ ModeCards() }
     `;
+}
+
+
+function handleWSMessage(msg: any) {
+  switch (msg.type) {
+    case "room:start":
+      localStorage.setItem("roomId", msg.roomId);
+      window.location.hash = `remote?room=${encodeURIComponent(msg.roomId)}`;
+      break;
+    case "invite:error":
+      alert("There are not players in this moment..");
+      break;
+  }
 }
